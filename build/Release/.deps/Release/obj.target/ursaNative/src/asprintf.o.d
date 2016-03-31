0eacab6463fb919414a6ac5862c5063e66a79b56
ebrain <ebrain@koiosoft>
2016-03-31 15:35:37 -0430

    se agrego el codigo de la libreria ursa

---
+++ build/Release/.deps/Release/obj.target/ursaNative/src/asprintf.o.d
@@ -0,0 +1,5 @@
+cmd_Release/obj.target/ursaNative/src/asprintf.o := g++ '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/home/ebrain/.node-gyp/0.10.37/src -I/home/ebrain/.node-gyp/0.10.37/deps/uv/include -I/home/ebrain/.node-gyp/0.10.37/deps/v8/include -I/home/ebrain/.node-gyp/0.10.37/deps/openssl/openssl/include  -fPIC -Wall -Wextra -Wno-unused-parameter -pthread -m64 -O2 -fno-strict-aliasing -fno-tree-vrp -fno-tree-sink -fno-omit-frame-pointer -fno-rtti -fno-exceptions -MMD -MF ./Release/.deps/Release/obj.target/ursaNative/src/asprintf.o.d.raw  -c -o Release/obj.target/ursaNative/src/asprintf.o ../src/asprintf.cc
+Release/obj.target/ursaNative/src/asprintf.o: ../src/asprintf.cc \
+ ../src/asprintf.h
+../src/asprintf.cc:
+../src/asprintf.h:
